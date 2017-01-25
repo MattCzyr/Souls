5456bdcb798e891a8c573748f1652116e25f3f04
mattczyr <mattczyr@gmail.com>
2017-01-25 00:05:00 -0500

    Increment version number

--- src/main/java/com/chaosthedude/souls/Souls.java
+++ src/main/java/com/chaosthedude/souls/Souls.java
@@ -28,7 +28,7 @@ public class Souls {
 
 	public static final String MODID = "souls";
 	public static final String NAME = "Souls";
-	public static final String VERSION = "1.1.3";
+	public static final String VERSION = "2.0.0";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
